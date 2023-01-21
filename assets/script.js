abfd8090db1bfcdec406bb2b1aee3bfa5a7562f1
myleen12 <myleen.santana12@icloud.com>
2023-01-21 10:38:16 -0800

    added button var and started first text question

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+var buttonA = document.getElementById('a');
+var buttonA = document.getElementById('b');
+var buttonA = document.getElementById('c');
+var buttonA = document.getElementById('d');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var testQuestions = [{
+Question: 'What tag do you put in HTML for Javascript?',
+ answerA: '<JS>',
+ answerB: '<scripting>',
+ answerC: '<HTML>',
+ answerD: '<script>',
+ correctAnswer: 'c'},
+{
+
+}]

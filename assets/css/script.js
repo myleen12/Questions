463e99af0b12f296dd02f9931d14d3e8d4c18c69
myleen12 <myleen.santana12@icloud.com>
2023-01-24 23:05:40 -0800

    added new function & var for question time display

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,21 +1,28 @@
-var buttonA = document.getElementById('a');
-var buttonB = document.getElementById('b');
-var buttonC = document.getElementById('c');
-var buttonD = document.getElementById('d');
 var quizTimer = document.getElementById('timer');
 var startbtn = document.getElementById('start-btn');
-var finalscore = document.getElementById('final score');
+var endingScore = document.getElementById('endingScore');
 var endbtn = document.getElementById('endbtn');
 var answerEl = document.getElementById('answer')
 var submitbtn = document.getElementById('submitbtn');
 var testQuestions = document.getElementById('testquestions');
 var countText = document.getElementById('countText');
 var questionText = document.getElementById('question');
+var result = document.querySelector('.result');
+
+var quizWrapper = document.querySelector('.quizWrapper');
+var gameOverContainer = document.querySelector('.gameOverContainer')
+
 var currentQuestion;
 var timer; 
 var count;
+
+var score = 0; 
+
 startbtn.addEventListener('click', startGame);
 
+gameOverContainer.style.display = 'none';
+
+var currentIndex = 0;
 
 var testQuestions = [
 {
@@ -25,16 +32,16 @@ var testQuestions = [
       {text: '<scripting>'},
       {text: '<HTML>'},
       {text: '<script>'}],
-    correctAnswer: '<script>'
-    },
-    {
+    correctAnswer: 'd'
+},
+{
     question: 'Who invented javascript?',
     answer: [
       {text: 'Brendan Eich'},
       {text: 'Beyonce'},
       {text: 'Jay Z'},
       {text: 'Elon Musk'}],
-    correctAnswer: 'Brendan Eich'
+    correctAnswer: 'a'
  },
  {
     question: 'What does DOM stand for?',
@@ -43,7 +50,7 @@ var testQuestions = [
       {text: 'Document object mood'},
       {text: 'Document or miles'},
       {text: 'Double stuff oreos and milk'}],
-    correctAnswer: 'Document Object Model'
+    correctAnswer: 'a'
  },
  {
     question: 'How many element can you use in HTML?',
@@ -52,7 +59,7 @@ var testQuestions = [
       {text: '500'},
       {text: 'unlimited'},
       {text: 'Only 5'}],
-    correctAnswer: 'unlimited'
+    correctAnswer: 'c'
 },
 {   
     question:'What is the prompt() used for?',
@@ -61,134 +68,79 @@ var testQuestions = [
       {text: 'To prompt user for to call a function'},
       {text: 'To make a title for HTML'},
       {text: 'To style CSS'}],
-    correctAnswer: 'Ask user for input'
-  }
+    correctAnswer: 'a'
+} 
 ];
 
+function displayQuestion() {
 
+    if (currentIndex === testQuestions.length) {
+        return showGameOverContainer();
+    }
 
-
-
-
-function displayQuestion0(currentQuestion) {
-    
-    currentQuestion = testQuestions[0].question;
-    questionText.innerText = currentQuestion;
-    buttonA.innerText = testQuestions[0].answer[0].text;
-    buttonB.innerText = testQuestions[0].answer[1].text;
-    buttonC.innerText = testQuestions[0].answer[2].text;
-    buttonD.innerText = testQuestions[0].answer[3].text;
-
-    buttonA.addEventListener('click', tapTimer);
-    buttonB.addEventListener('click', tapTimer);
-    buttonC.addEventListener('click', tapTimer);
-    buttonD.addEventListener('click', displayQuestion1);
-    
+    questionText.innerText = testQuestions[currentIndex].question;
+    buttonA.innerText = testQuestions[currentIndex].answer[0].text;
+    buttonB.innerText = testQuestions[currentIndex].answer[1].text;
+    buttonC.innerText = testQuestions[currentIndex].answer[2].text;
+    buttonD.innerText = testQuestions[currentIndex].answer[3].text;
 
 };
 
-function displayQuestion1(currentQuestion) {
-    
-    currentQuestion = testQuestions[1].question;
-    questionText.innerText = currentQuestion;
-    buttonA.innerText = testQuestions[1].answer[0].text;
-    buttonB.innerText = testQuestions[1].answer[1].text;
-    buttonC.innerText = testQuestions[1].answer[2].text;
-    buttonD.innerText = testQuestions[1].answer[3].text;
+function showGameOverContainer() {
+    endingScore.textContent = score;
 
-    buttonA.addEventListener('click', displayQuestion2);
-    buttonB.addEventListener('click', tapTimer);
-    buttonC.addEventListener('click', tapTimer);
-    buttonD.addEventListener('click', tapTimer);
-
-
-};
-function displayQuestion2(currentQuestion) {
-    
-    currentQuestion = testQuestions[2].question;
-    questionText.innerText = currentQuestion;
-    buttonA.innerText = testQuestions[2].answer[0].text;
-    buttonB.innerText = testQuestions[2].answer[1].text;
-    buttonC.innerText = testQuestions[2].answer[2].text;
-    buttonD.innerText = testQuestions[2].answer[3].text;
-
-    buttonA.addEventListener('click', displayQuestion3);
-    buttonB.addEventListener('click', tapTimer);
-    buttonC.addEventListener('click', tapTimer);
-    buttonD.addEventListener('click', tapTimer);
-
-};
-function displayQuestion3(currentQuestion) {
-    
-    currentQuestion = testQuestions[3].question;
-    questionText.innerText = currentQuestion;
-    buttonA.innerText = testQuestions[3].answer[0].text;
-    buttonB.innerText = testQuestions[3].answer[1].text;
-    buttonC.innerText = testQuestions[3].answer[2].text;
-    buttonD.innerText = testQuestions[3].answer[3].text;
-
-    buttonA.addEventListener('click', tapTimer);
-    buttonB.addEventListener('click', tapTimer);
-    buttonC.addEventListener('click', displayQuestion4);
-    buttonD.addEventListener('click', tapTimer);
-
-};
-function displayQuestion4(currentQuestion) {
-    
-    currentQuestion = testQuestions[4].question;
-    questionText.innerText = currentQuestion;
-    buttonA.innerText = testQuestions[4].answer[0].text;
-    buttonB.innerText = testQuestions[4].answer[1].text;
-    buttonC.innerText = testQuestions[4].answer[2].text;
-    buttonD.innerText = testQuestions[4].answer[3].text;
-
-    buttonA.addEventListener('click', winGame);
-    buttonB.addEventListener('click', tapTimer);
-    buttonC.addEventListener('click', tapTimer);
-    buttonD.addEventListener('click', tapTimer);
-
-};
-
-function winGame() {
-    currentQuestion.classList.add('hide')
-    alert('Congrats You Won!')
+    quizWrapper.style.display = 'none';
+    gameOverContainer.style.display = 'block';
 }
 
+function checkAnswer(response) {
+
+    console.log("checking answer...")
+
+    var correctAnswer = testQuestions[currentIndex].correctAnswer;
+
+    if (correctAnswer === response) {
+        score++;
+        result.textContent = "Correct!";
+    } else {
+        count -= 5;
+        result.textContent = "Wrong!";
+    }
+
+    currentIndex++;
+    displayQuestion();
+
+    setTimeout(function() {
+        result.textContent = "";
+    }, 1000);
+
+}
 function startGame() {
     isWin = false; 
     count = 60;
     startbtn.disabled = true; 
+    displayQuestion();
     startTimer();
-    console.log('hello')
-    displayQuestion0();
 };
 
 function startTimer() {
     timer = setInterval(function () {
         count--;
         countText.textContent = count;
-        if (count >= 0) {
-            if (count > 0) {
-                clearInterval(timer);
-                winGame();
-            }
-        }
-        if (count === 0) {
+        
+        if (count < 0 && currentIndex !== testQuestions.length) {
             clearInterval(timer);
-            loseGame();
         }
     }, 1000);
     console.log('hello again')
 }
- function loseGame() {
-    alert('YOU ARE FINISHED!!!')
 
- }
+
 
 
  function tapTimer() {
     if (count < 6) {
-        count === 0
+        count = 0
     }
     else {
         count = count -5
